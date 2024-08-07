82a991b0949d35a3b06992965f8a398e700281b9
Washio Yusaku <washioyusaku@icloud.com>
2024-08-08 00:35:20 +0900

    init: cdk

---
+++ test/cdk_appsync_dynamodb.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkAppsyncDynamodb from '../lib/cdk_appsync_dynamodb-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_appsync_dynamodb-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkAppsyncDynamodb.CdkAppsyncDynamodbStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
