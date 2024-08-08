a0a48ca738f42174464ab34e7d7c6128901dfb0c
Washio Yusaku <washioyusaku@icloud.com>
2024-08-09 02:35:26 +0900

    add: AppSync API エンドポイント

--- lib/cdk_appsync_dynamodb-stack.ts
+++ lib/cdk_appsync_dynamodb-stack.ts
@@ -1,16 +1,31 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as appsync from "aws-cdk-lib/aws-appsync";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import {Construct} from "constructs";
 
 export class CdkAppsyncDynamodbStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // makes a GraphQL API
+    const api = new appsync.GraphqlApi(this, "post-apis", {
+      name: "api-to-process-posts",
+      schema: appsync.SchemaFile.fromAsset("schema/schema.graphql"),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkAppsyncDynamodbQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Prints out URL
+    new cdk.CfnOutput(this, "GraphQL API URL", {
+      value: api.graphqlUrl,
+    });
+
+    // Prints out the AppSync GraphQL API key to the terminal
+    new cdk.CfnOutput(this, "GraphQL API Key", {
+      value: api.apiKey || "",
+    });
+
+    // Prints out the stack region to the terminal
+    new cdk.CfnOutput(this, "Stack Region", {
+      value: this.region,
+    });
   }
 }
