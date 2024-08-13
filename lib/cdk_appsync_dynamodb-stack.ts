1172e814f74402b8a57447ca108cbcd0519ba4d3
Washio Yusaku <washioyusaku@icloud.com>
2024-08-13 22:01:58 +0900

    add: AppSync設定を追加

--- lib/cdk_appsync_dynamodb-stack.ts
+++ lib/cdk_appsync_dynamodb-stack.ts
@@ -13,6 +13,88 @@ export class CdkAppsyncDynamodbStack extends cdk.Stack {
       schema: appsync.SchemaFile.fromAsset("schema/schema.graphql"),
     });
 
+    // creates a DDB table
+    const add_ddb_table = new dynamodb.Table(this, "posts-table", {
+      partitionKey: {
+        name: "id",
+        type: dynamodb.AttributeType.STRING,
+      },
+    });
+
+    // Creates a function for query
+    const add_func = new appsync.AppsyncFunction(this, "func-get-post", {
+      name: "get_posts_func_1",
+      api,
+      dataSource: api.addDynamoDbDataSource("table-for-posts", add_ddb_table),
+      code: appsync.Code.fromInline(`
+          export function request(ctx) {
+          return { operation: 'Scan' };
+          }
+
+          export function response(ctx) {
+          return ctx.result.items;
+          }
+      `),
+      runtime: appsync.FunctionRuntime.JS_1_0_0,
+    });
+
+    // Creates a function for mutation
+    const add_func_2 = new appsync.AppsyncFunction(this, "func-add-post", {
+      name: "add_posts_func_1",
+      api,
+      dataSource: api.addDynamoDbDataSource("table-for-posts-2", add_ddb_table),
+      code: appsync.Code.fromInline(`
+          export function request(ctx) {
+            return {
+            operation: 'PutItem',
+            key: util.dynamodb.toMapValues({id: util.autoId()}),
+            attributeValues: util.dynamodb.toMapValues(ctx.args.input),
+            };
+          }
+
+          export function response(ctx) {
+            return ctx.result;
+          }
+      `),
+      runtime: appsync.FunctionRuntime.JS_1_0_0,
+    });
+
+    // Adds a pipeline resolver with the get function
+    new appsync.Resolver(this, "pipeline-resolver-get-posts", {
+      api,
+      typeName: "Query",
+      fieldName: "getPost",
+      code: appsync.Code.fromInline(`
+          export function request(ctx) {
+          return {};
+          }
+
+          export function response(ctx) {
+          return ctx.prev.result;
+          }
+      `),
+      runtime: appsync.FunctionRuntime.JS_1_0_0,
+      pipelineConfig: [add_func],
+    });
+
+    // Adds a pipeline resolver with the create function
+    new appsync.Resolver(this, "pipeline-resolver-create-posts", {
+      api,
+      typeName: "Mutation",
+      fieldName: "createPost",
+      code: appsync.Code.fromInline(`
+          export function request(ctx) {
+          return {};
+          }
+
+          export function response(ctx) {
+          return ctx.prev.result;
+          }
+      `),
+      runtime: appsync.FunctionRuntime.JS_1_0_0,
+      pipelineConfig: [add_func_2],
+    });
+
     // Prints out URL
     new cdk.CfnOutput(this, "GraphQL API URL", {
       value: api.graphqlUrl,
